55b06160226418ad65476e9edc392a3b82717c18
Maximiliano Ferradás <95547687+mferradas@users.noreply.github.com>
2022-03-28 23:56:56 -0300

    Add files via upload

---
+++ 6_guess_number.py
@@ -0,0 +1,56 @@
+'''
+
+EJERCICIO: 
+
+1) Pedir nombre al usuario
+2) Crear un número aleatorio entre 1 y 100
+3) 8 intentos para adivinarlo
+4) Pedir números hasta agotar los 8 intentos
+
+En cada intento, el programa debe responder 4 cosas distinas:
+
+    - Menos de 1 o más de 100 -> Fuera de rango
+    - Número menor al n° secreto
+    - Número mayor al n° secreto
+    - si el usuario acertó:
+        - se le informa que ha ganado
+        - cuántos intentos le ha tomado
+        
+'''
+
+from random import *
+
+random = randint(1, 101)
+
+attempts = 0
+
+print('')
+print('¡¡Bienvenido!!')
+print('')
+print('El programa genera un número secreto entre 1 y 100, y tienes 8 intentos para adivinarlo.')
+print('')
+print('¡¡Mucha suerte!!')
+print('')
+
+number = int(input('Ingresa un número entre 1 y 100 (8 intentos): '))
+
+while number > 100 or number < 1:
+    number = int(input('Fuera de rango. Ingresa otro número: '))        
+else:
+    print('Número dentro de rango.')
+
+while number != random:
+    if number > random:
+        print('Número mayor al N° secreto.')
+    elif number < random:
+        print('Número menor al N° secreto.')
+
+    attempts = attempts + 1
+    attempts_rev = 8 - attempts
+    if attempts_rev == 0:
+        print('Te has quedado sin intentos.')
+        break
+    number = int(input(f'No has acertado. Te quedan {attempts_rev} intentos: '))
+else:
+    print('¡¡Has acertado el número!! ¡¡Felicitaciones!!')
+    print(f'Lo lograste en {attempts} intentos.')
